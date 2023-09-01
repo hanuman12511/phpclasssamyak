bde96369bdf77cbe2e392f4d937c90c7d020d334
hanuman12511 <er.hanumansaini@gmail.com>
2023-09-01 21:04:51 +0530

    sdsd

--- website/index.php
+++ website/index.php
@@ -1,16 +1,46 @@
+<style>
+    .img{width:100px}
+    </style>
+
 <?php
 include('../connection/connection.php');
-    
+$product = array();   
 $sql = "select *from product";
 $r = $conn->query($sql);
-$product=[];
-while($row=$r->fetch_assoc()){
-   // print_r($row) ;
-   $product=$row;
-   /*  $pname = $row['pname'];
-    $prate = $row['prate'];
-    $pqty = $row['pqty']; */
+$itemCount = $r->num_rows;
+if($itemCount>0){
+ 
+  $product['body'] = array();
+  $product['itemcount'] = $itemCount;
+
+  while($row = $r->fetch_ASSOC()){
+      extract($row);
+      $e = array(
+       
+        "pname"=>$pname,
+        "prate"=>$prate,
+        "pqty"=>$pqty,
+        "image"=>$image,
+);
+      array_push($product['body'],$e);
 }
-print_r($product);
-echo$product['pname'];
+ 
+   json_encode($product["body"]);
+
+
+
+}
+$item =  $product["body"];
+    for($i=0;$i<count($item);$i++){
+      $pname =$item[$i]['pname'];
+      $img =$item[$i]['image'];
+        echo "<div>";
+        echo "<img src='../admin/images/$img' class='img'/>";
+        echo "<p>$pname</p>";
+        
+        echo "</div>";
+    }
+
+
+
 ?>
